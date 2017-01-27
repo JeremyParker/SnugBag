d5a0a3d45788b91cb1df909b5ea427b977ad1618
Jeremy Parker <github@jeremyparker.org>
2017-01-27 10:30:42 -0500

    Add Dummy Teams Endpoint

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,68 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :update, :destroy]
+
+  # GET /teams
+  def index
+    @teams = [
+        {
+          :id => 1,
+          :display_name => "discovery"
+        },
+        {
+          :id => 2,
+          :display_name => "create"
+        },
+        {
+          :id => 3,
+          :display_name => "events"
+        },
+        {
+          :id => 4,
+          :display_name => "international/payments"
+        },
+    ]
+
+    render json: @teams
+  end
+
+  # GET /teams/1
+  def show
+    render json: @team
+  end
+
+  # POST /teams
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      render json: @team, status: :created, location: @team
+    else
+      render json: @team.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /teams/1
+  def update
+    if @team.update(team_params)
+      render json: @team
+    else
+      render json: @team.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /teams/1
+  def destroy
+    @team.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_team
+      @team = Team.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def team_params
+      params.fetch(:team, {})
+    end
+end
